a53ef2c49fb583b1209dbfd2a7404d2aa996ee46
Abhijit Kote <abhijitkote40@gmail.com>
2026-01-21 14:02:56 +0000

    Initial success

--- Project1/index.js
+++ Project1/index.js
@@ -1,3 +1,9 @@
-//Project success
-var a=5,b=6;
-console.log(a+b);
+const accounId=123;
+let accountEmail="abhi@gmail.com";
+var accountPass="12345";
+accountCity = "Jaysingpur"
+
+accountEmail = "abc@gmail.com";
+accountPass = "1212";
+accountCity = "Gadhinglaj";
+console.table([accounId, accountEmail, accountPass, accountCity]);
