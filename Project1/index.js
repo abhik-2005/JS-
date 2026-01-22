ebed424544dfea299f6454c7249eb1bdb5f07149
Abhijit Kote <abhijitkote40@gmail.com>
2026-01-22 14:13:00 +0000

    day3

--- Project1/index.js
+++ Project1/index.js
@@ -1,9 +1,19 @@
-const accounId=123;
-let accountEmail="abhi@gmail.com";
-var accountPass="12345";
-accountCity = "Jaysingpur"
+// const accounId=123;
+// let accountEmail="abhi@gmail.com";
+// var accountPass="12345";
+// accountCity = "Jaysingpur"
 
-accountEmail = "abc@gmail.com";
-accountPass = "1212";
-accountCity = "Gadhinglaj";
-console.table([accounId, accountEmail, accountPass, accountCity]);
+// accountEmail = "abc@gmail.com";
+// accountPass = "1212";
+// accountCity = "Gadhinglaj";
+// console.table([accounId, accountEmail, accountPass, accountCity]);
+
+// console.log(null > 0);
+// console.log(null ==0);
+
+// console.log(null >=0);
+
+let stack = "abhi";
+let stack1 = stack;
+console.log(stack1);
+
