aae7a8a86840f30889b8940782ed7e1336078d56
Abhijit Kote <abhijitkote40@gmail.com>
2026-03-23 17:41:43 +0000

    Done

--- Project1/index.js
+++ Project1/index.js
@@ -13,7 +13,24 @@
 
 // console.log(null >=0);
 
-let stack = "abhi";
-let stack1 = stack;
-console.log(stack1);
 
+// let stack = "abhi";
+// let stack1 = stack;
+// console.log(stack1); // let is used instead of var because its new version why because https://www.youtube.com/watch?v=watN0fbZeI0 . 
+
+
+// const a="hi"
+// console.log(`${a} its abhi`) //string interpolation (*note = '' and `` are not same)
+
+//prototype
+// const a="hello"
+// console.log(a.__proto__) //output is {} but  {h e l l o} 
+
+//---------------Maths---------------//
+
+//random function is very imortant in js maths
+
+const min = 1
+const max = 6
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+//dice
