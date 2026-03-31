a4bb9c8643f2637171e7b25096e74e0062e1ab47
Abhijit Kote <abhijitkote40@gmail.com>
2026-03-31 19:01:32 +0000

    ok

--- Project1/index.js
+++ Project1/index.js
@@ -30,7 +30,11 @@
 
 //random function is very imortant in js maths
 
-const min = 1
-const max = 6
-console.log(Math.floor(Math.random() * (max - min + 1)) + min)
 //dice
+// const min = 1
+// const max = 6
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+//date
+let md=new Date()
+console.log(md)
